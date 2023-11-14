8bdbf977679e0d01278a17f3b48a8781f74f01da
Zafer <test01012018@gmail.com>
2023-11-14 23:51:00 +0100

    14.11.2023

---
+++ src/test/java/stepdefs/Stepdefs_Zafer.java
@@ -0,0 +1,105 @@
+package stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Stepdefs_Zafer {
+    @Given("der Benutzer die Amazon-Homepage aufruft")
+    public void derBenutzerDieAmazonHomepageAufruft() {
+    }
+
+    @And("sollte der Benutzer auf der Amazon-Homepage landen")
+    public void sollteDerBenutzerAufDerAmazonHomepageLanden() {
+    }
+
+    @When("der Benutzer das Dropdown-Menü neben der Suchleiste überprüft")
+    public void derBenutzerDasDropdownMenüNebenDerSuchleisteÜberprüft() {
+    }
+
+    @Then("sollte das Dropdown-Menü sichtbar und anklickbar sein")
+    public void sollteDasDropdownMenüSichtbarUndAnklickbarSein() {
+    }
+
+    @And("das Dropdown-Menü sollte die Kategorie mit dem Titel {string} enthalten")
+    public void dasDropdownMenüSollteDieKategorieMitDemTitelEnthalten(String arg0) {
+    }
+
+    @When("der Benutzer eine Auswahl aus dem Dropdown-Menü trifft")
+    public void derBenutzerEineAuswahlAusDemDropdownMenüTrifft() {
+    }
+
+    @Then("sollte sich das Dropdown-Menü schließen")
+    public void sollteSichDasDropdownMenüSchließen() {
+    }
+
+    @And("der Benutzer sollte zum Suchfeld weitergeleitet werden")
+    public void derBenutzerSollteZumSuchfeldWeitergeleitetWerden() {
+    }
+
+    @When("der Benutzer das Produkt in das Suchfeld eingibt")
+    public void derBenutzerDasProduktInDasSuchfeldEingibt() {
+    }
+
+    @Then("sollte unten ein Dropdown-Menü mit Optionen angezeigt werden")
+    public void sollteUntenEinDropdownMenüMitOptionenAngezeigtWerden() {
+    }
+
+    @And("die Optionen sollten dem Namen des gesuchten Produkts entsprechen")
+    public void dieOptionenSolltenDemNamenDesGesuchtenProduktsEntsprechen() {
+    }
+
+    @And("sei der Benutzer auf der Amazon-Homepage")
+    public void seiDerBenutzerAufDerAmazonHomepage() {
+    }
+
+    @Then("das Dropdown-Menü neben der Suchleiste ist sichtbar und anklickbar")
+    public void dasDropdownMenüNebenDerSuchleisteIstSichtbarUndAnklickbar() {
+    }
+
+    @And("das System sollte die Seite aktualisieren, um zum gewünschten Produkt zu gelangen")
+    public void dasSystemSollteDieSeiteAktualisierenUmZumGewünschtenProduktZuGelangen() {
+    }
+
+    @And("der Benutzer hat eine Auswahl aus dem Dropdown-Menü getroffen")
+    public void derBenutzerHatEineAuswahlAusDemDropdownMenüGetroffen() {
+    }
+
+    @Then("sollte eine Liste relevanter Ergebnisse oben links angezeigt werden")
+    public void sollteEineListeRelevanterErgebnisseObenLinksAngezeigtWerden() {
+    }
+
+    @And("die Meldung {string} von mehr als {string} Ergebnissen oder Vorschlägen für {string} sollte erscheinen")
+    public void dieMeldungVonMehrAlsErgebnissenOderVorschlägenFürSollteErscheinen(String arg0, String arg1, String arg2) {
+    }
+
+    @Then("sollte das System verschiedene Faktoren wie Produktnamen, Verfügbarkeit und Kundenbewertungen berücksichtigen")
+    public void sollteDasSystemVerschiedeneFaktorenWieProduktnamenVerfügbarkeitUndKundenbewertungenBerücksichtigen() {
+    }
+
+    @And("eine Liste relevanter Suchergebnisse bereitstellen")
+    public void eineListeRelevanterSuchergebnisseBereitstellen() {
+    }
+
+    @Then("sollte das System in allen Kategorien nach dem Produkt suchen")
+    public void sollteDasSystemInAllenKategorienNachDemProduktSuchen() {
+
+    }
+
+    @And("der Benutzer hat das Produkt in das Suchfeld eingegeben")
+    public void derBenutzerHatDasProduktInDasSuchfeldEingegeben() {
+    }
+
+    @And("das gesuchte Produkt befindet sich nicht in der gewünschten Kategorie")
+    public void dasGesuchteProduktBefindetSichNichtInDerGewünschtenKategorie() {
+    }
+
+    @Then("sollte sich die im Dropdown-Menü ausgewählte Kategorie nicht ändern")
+    public void sollteSichDieImDropdownMenüAusgewählteKategorieNichtÄndern() {
+    }
+
+    @And("links oben den Suchergebnissen sollte die Meldung {string} und {string} angezeigt werden")
+    public void linksObenDenSuchergebnissenSollteDieMeldungUndAngezeigtWerden(String arg0, String arg1) {
+    }
+}
--- src/test/resources/features/KPRNTB-T66_Verbesserte_keine_Suchgenauigkeit.feature
+++ src/test/resources/features/KPRNTB-T66_Verbesserte_keine_Suchgenauigkeit.feature
@@ -1,17 +1,36 @@
 Feature: KPRNTB-136 Verbesserte Suchgenauigkeit
 
   Scenario: Verbesserte keine Suchgenauigkeit
-#  Benutzer landet auf der Amazon-Homepage.
-#  Links neben der Suchleiste sollte ein Dropdown-Menü zum Durchsuchen von Kategorien mit dem Titel „Alle“ sichtbar und anklickbar sein.
-    Given Benutzer landet auf der Amazon-Homepage
-    Then Anasayfada title da "Amazon" oldugu gorulur
-    Then Alle Dropdown-Menü sollte sichtbar und anklickbar
-    And Alle menüsüne tıklanır ve bir kategori secilir
-    Then Alle menüsünün kapandığı görülür
-    And
-
-#    Der Benutzer wählt zunächst die Kategorie des Produkts aus, nach der er suchen möchte, im Kategorie-Dropdown-Menü mit dem Titel „Alle“ links neben der Suchleiste.
-#  Nachdem Sie eine Auswahl aus dem Dropdown-Menü getroffen haben, sollte sich das Dropdown-Menü schließen.
-#
-#  Das System leitet den Benutzer zur Suchfeld weiter, um das Produkt einzugeben, nach dem er suchen möchte.
-#  Wenn Sie das Produkt, nach dem Sie suchen möchten, in das Suchfeld eingeben, sollte unten ein Dropdown-Menü mit Optionen angezeigt werden, die dem Namen des gesuchten Produkts entsprechen.
+    Given der Benutzer die Amazon-Homepage aufruft
+    And   sollte der Benutzer auf der Amazon-Homepage landen
+    When  der Benutzer das Dropdown-Menü neben der Suchleiste überprüft
+    Then  sollte das Dropdown-Menü sichtbar und anklickbar sein
+    And   das Dropdown-Menü sollte die Kategorie mit dem Titel "Alle" enthalten
+    When  der Benutzer eine Auswahl aus dem Dropdown-Menü trifft
+    Then  sollte sich das Dropdown-Menü schließen
+    And   der Benutzer sollte zum Suchfeld weitergeleitet werden
+    When  der Benutzer das Produkt in das Suchfeld eingibt
+    Then  sollte unten ein Dropdown-Menü mit Optionen angezeigt werden
+    And   die Optionen sollten dem Namen des gesuchten Produkts entsprechen
+    And   sei der Benutzer auf der Amazon-Homepage
+    Then  das Dropdown-Menü neben der Suchleiste ist sichtbar und anklickbar
+    When  der Benutzer eine Auswahl aus dem Dropdown-Menü trifft
+    Then  sollte sich das Dropdown-Menü schließen
+    And   das System sollte die Seite aktualisieren, um zum gewünschten Produkt zu gelangen
+    Then  sei der Benutzer auf der Amazon-Homepage
+    And   der Benutzer hat eine Auswahl aus dem Dropdown-Menü getroffen
+    When  der Benutzer das Produkt in das Suchfeld eingibt
+    Then  sollte eine Liste relevanter Ergebnisse oben links angezeigt werden
+    And   die Meldung "Seitenzahl" von mehr als "Produktmenge" Ergebnissen oder Vorschlägen für "gesuchte Produkt" sollte erscheinen
+    Then  sei der Benutzer auf der Amazon-Homepage
+    When  der Benutzer das Produkt in das Suchfeld eingibt
+    Then  sollte das System verschiedene Faktoren wie Produktnamen, Verfügbarkeit und Kundenbewertungen berücksichtigen
+    And   eine Liste relevanter Suchergebnisse bereitstellen
+    Then  sei der Benutzer auf der Amazon-Homepage
+    And   der Benutzer hat das Produkt in das Suchfeld eingegeben
+    And   das gesuchte Produkt befindet sich nicht in der gewünschten Kategorie
+    Then  sollte das System in allen Kategorien nach dem Produkt suchen
+    And   sei der Benutzer auf der Amazon-Homepage
+    When  der Benutzer eine Auswahl aus dem Dropdown-Menü trifft
+    Then  sollte sich die im Dropdown-Menü ausgewählte Kategorie nicht ändern
+    And   links oben den Suchergebnissen sollte die Meldung "Ergebnisse" und "Mehr über diese Ergebnisse" angezeigt werden
