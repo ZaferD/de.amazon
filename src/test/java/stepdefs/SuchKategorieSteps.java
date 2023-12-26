469f0407825c37964c690f288252f615a353d153
ZaferD <test01012018@gmail.com>
2023-12-26 18:20:44 +0100

    26.12.2023

---
+++ src/test/java/stepdefs/SuchKategorieSteps.java
@@ -0,0 +1,99 @@
+package stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class SuchKategorieSteps extends BaseSteps{
+    @Given("ich gehe auf die Webseite Amazon-Homepage amazon.de")
+    public void ichGeheAufDieWebseiteAmazonHomepageAmazonDe() {
+    }
+
+    @Then("Ich sehe den Titel auf Amazon.de.")
+    public void ichSeheDenTitelAufAmazonDe() {
+    }
+
+    @And("ich klicke auf Cookies akzeptieren")
+    public void ichKlickeAufCookiesAkzeptieren() {
+    }
+
+    @Then("ich sehe den Text Alle, oben links auf der Seite, unter amazon.de Logo")
+    public void ichSeheDenTextAlleObenLinksAufDerSeiteUnterAmazonDeLogo() {
+    }
+
+    @And("ich klicke auf den Text Alle")
+    public void ichKlickeAufDenTextAlle() {
+    }
+
+    @Then("ich sehe die Kategorienlist")
+    public void ichSeheDieKategorienlist() {
+    }
+
+    @And("ich sehe auf die Kategorie Bücher in der List")
+    public void ichSeheAufDieKategorieBücherInDerList() {
+    }
+
+    @Then("ich klicke auf die Kategorie Bücher in der List")
+    public void ichKlickeAufDieKategorieBücherInDerList() {
+    }
+
+    @And("ich sehe Alle Bücher Title")
+    public void ichSeheAlleBücherTitle() {
+    }
+
+    @Then("ich klicke auf Alle Bücher Text")
+    public void ichKlickeAufAlleBücherText() {
+    }
+
+    @And("ich sehe die Liste der Kategorien unter dem Titel Stöbern links auf der Seite")
+    public void ichSeheDieListeDerKategorienUnterDemTitelStöbernLinksAufDerSeite() {
+    }
+
+    @And("ich sehe auf der Seite Vorgestellte Kategorien Titel")
+    public void ichSeheAufDerSeiteVorgestellteKategorienTitel() {
+    }
+
+    @And("ich gehe erste Kategorie unter Vorgestellte Kategorien Titel")
+    public void ichGeheErsteKategorieUnterVorgestellteKategorienTitel() {
+    }
+
+    @And("ich klicke in der sich öffnenden Kategorienliste auf die erste Kategorie")
+    public void ichKlickeInDerSichÖffnendenKategorienlisteAufDieErsteKategorie() {
+    }
+
+    @Then("ich sehe die Sprache Text in der Liste links auf der Seite")
+    public void ichSeheDieSpracheTextInDerListeLinksAufDerSeite() {
+    }
+
+    @And("ich klicke auf Deutsch Checkbox")
+    public void ichKlickeAufDeutschCheckbox() {
+    }
+
+    @Then("ich sehe oben links auf der Seite Seitenzahl der Seiten mehr als Ergebnissen")
+    public void ichSeheObenLinksAufDerSeiteSeitenzahlDerSeitenMehrAlsErgebnissen() {
+    }
+
+    @And("ich sehe die Ergebnisse Title auf der Seite")
+    public void ichSeheDieErgebnisseTitleAufDerSeite() {
+    }
+
+    @Then("ich sehe unter Format Text in der Liste links auf der Seite")
+    public void ichSeheUnterFormatTextInDerListeLinksAufDerSeite() {
+    }
+
+    @And("ich klicke Hörbuch Checkbox")
+    public void ichKlickeHörbuchCheckbox() {
+    }
+
+    @Then("ich sehe oben links auf der Seite Seitenzahl der Seiten von mehr als Ergebnissen")
+    public void ichSeheObenLinksAufDerSeiteSeitenzahlDerSeitenVonMehrAlsErgebnissen() {
+    }
+
+    @Then("ich sehe unter der Kundenrezensionen Text in der Liste links auf der Seite")
+    public void ichSeheUnterDerKundenrezensionenTextInDerListeLinksAufDerSeite() {
+    }
+
+    @And("ich klicke vier Sterne & mehr")
+    public void ichKlickeVierSterneMehr() {
+    }
+}
--- src/test/resources/features/KPRNTB-T99_(Automation)DieFunktionalitätDerKategorieBuecher.feature
+++ src/test/resources/features/KPRNTB-T99_(Automation)DieFunktionalitätDerKategorieBuecher.feature
@@ -3,7 +3,7 @@ Feature: KPRNTB-139 Erweiterung Suchkategorie
 
   Scenario: (Automation) Die Funktionalität der Kategorie "Bücher"
     Given ich gehe auf die Webseite Amazon-Homepage amazon.de
-    Then ich sehe title Amazon.de
+    Then Ich sehe den Titel auf Amazon.de.
     And ich klicke auf Cookies akzeptieren
     Then ich sehe den Text Alle, oben links auf der Seite, unter amazon.de Logo
     And ich klicke auf den Text Alle
@@ -13,15 +13,9 @@ Feature: KPRNTB-139 Erweiterung Suchkategorie
     And ich sehe Alle Bücher Title
     Then ich klicke auf Alle Bücher Text
     And ich sehe die Liste der Kategorien unter dem Titel Stöbern links auf der Seite
-    And ich sehe Links auf der geöffneten Seite eine Liste von Bücherkategorien
     And ich sehe auf der Seite Vorgestellte Kategorien Titel
-    And ich sehe die Kategorien unter Vorgestellte Kategorien Titel
-    Then ich sehe, dass, wenn ich den Mauszeiger über die erste dieser Kategorien bewege, eine Liste von Kategorien angezeigt wird
-    And ich klicke auf erste Kategorie
-    And ich gehe zur vorherigen Seite
-    Then ich sehe unten auf der Seite den Text Zurück zum Seitenanfang
-    And ich klicke auf den Text Zurück zum Seitenanfang
-    And ich sehe auf der Seite Bücher Title
+    And ich gehe erste Kategorie unter Vorgestellte Kategorien Titel
+    And ich klicke in der sich öffnenden Kategorienliste auf die erste Kategorie
     Then ich sehe die Sprache Text in der Liste links auf der Seite
     And ich klicke auf Deutsch Checkbox
     Then ich sehe oben links auf der Seite Seitenzahl der Seiten mehr als Ergebnissen
@@ -30,7 +24,7 @@ Feature: KPRNTB-139 Erweiterung Suchkategorie
     And ich klicke Hörbuch Checkbox
     Then ich sehe oben links auf der Seite Seitenzahl der Seiten von mehr als Ergebnissen
     And ich sehe die Ergebnisse Title auf der Seite
-    Then ich sehe unter Kundenrezensionen Text in der Liste links auf der Seite
+    Then ich sehe unter der Kundenrezensionen Text in der Liste links auf der Seite
     And ich klicke vier Sterne & mehr
     Then ich sehe oben links auf der Seite Seitenzahl der Seiten von mehr als Ergebnissen
     And ich sehe die Ergebnisse Title auf der Seite
